e571c127fbab7cfba005ce0aaa5ea236a054352a
Matthias Reitinger <reitinge@in.tum.de>
2010-04-27 15:47:44 +0200

    Converted tabs to spaces

--- src/de/tum/in/jrealityplugin/JRealityPlugin.java
+++ src/de/tum/in/jrealityplugin/JRealityPlugin.java
@@ -8,30 +8,30 @@ import de.cinderella.api.cs.CindyScriptPlugin;
  */
 public class JRealityPlugin extends CindyScriptPlugin {
 
-	/* (non-Javadoc)
-	 * @see de.cinderella.api.cs.CindyScriptPlugin#getAuthor()
-	 */
-	@Override
-	public String getAuthor() {		
-		return "Jan Sommer und Matthias Reitinger";
-	}
+    /* (non-Javadoc)
+     * @see de.cinderella.api.cs.CindyScriptPlugin#getAuthor()
+     */
+    @Override
+    public String getAuthor() {
+        return "Jan Sommer und Matthias Reitinger";
+    }
 
-	/* (non-Javadoc)
-	 * @see de.cinderella.api.cs.CindyScriptPlugin#getName()
-	 */
-	@Override
-	public String getName() {
-		return "jReality for Cinderella";
-	}
+    /* (non-Javadoc)
+     * @see de.cinderella.api.cs.CindyScriptPlugin#getName()
+     */
+    @Override
+    public String getName() {
+        return "jReality for Cinderella";
+    }
 
-	/**
-	 * Squares the given number
-	 * @param x
-	 * @return The square of x
-	 */
-	@CindyScript("square")
-	public double square(double x) {
-		return x*x;
-	}
+    /**
+     * Squares the given number
+     * @param x
+     * @return The square of x
+     */
+    @CindyScript("square")
+    public double square(double x) {
+        return x*x;
+    }
 
 }
