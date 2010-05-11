a13388e9102d72e521dbcdf254331b6a3b18e603
Jan Sommer <sommerj@in.tum.de>
2010-05-11 11:08:39 +0200

    Not working point drawing routine added

--- src/de/tum/in/jrealityplugin/JRealityViewer.java
+++ src/de/tum/in/jrealityplugin/JRealityViewer.java
@@ -1,6 +1,7 @@
 package de.tum.in.jrealityplugin;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 import de.jreality.geometry.PointSetFactory;
 import de.jreality.scene.SceneGraphComponent;
@@ -8,15 +9,28 @@ import de.jreality.ui.viewerapp.ViewerApp;
 
 @SuppressWarnings("deprecation")
 public class JRealityViewer implements Cindy3DViewer {
-    private PointSetFactory psf;
-    private SceneGraphComponent sceneRoot;
     private ViewerApp viewer;
     
+    private SceneGraphComponent sceneRoot; 
+    
+    // Point resources
+    private SceneGraphComponent scenePoints;
+    private PointSetFactory psf;
+    private ArrayList<double[]> pointCoordinates;
+    private ArrayList<Color> pointColors;
+    
     public JRealityViewer() {
+    	pointCoordinates = new ArrayList<double[]>();
+    	pointColors = new ArrayList<Color>();
+    	
         psf = new PointSetFactory();
 
         sceneRoot = new SceneGraphComponent();
-        sceneRoot.setGeometry(psf.getGeometry());
+        
+        scenePoints = new SceneGraphComponent();
+        scenePoints.setGeometry(psf.getGeometry());
+        
+        sceneRoot.addChild(scenePoints);
 
         viewer = new ViewerApp(sceneRoot);
         viewer.setAttachBeanShell(false);
@@ -30,18 +44,22 @@ public class JRealityViewer implements Cindy3DViewer {
 
     @Override
     public void begin() {
+    	pointCoordinates.clear();
+    	pointColors.clear();
     }
 
     @Override
     public void end() {
+    	psf.setVertexCoordinates((double[][])pointCoordinates.toArray());
+    	
+    	psf.setVertexCount(pointCoordinates.size());
+    	psf.update();
         viewer.getFrame().setVisible(true);
     }
 
     @Override
     public void addPoint(double x, double y, double z) {
-        psf.setVertexCount(1);
-        psf.setVertexCoordinates(new double[] { x, y, z });
-        psf.update();
+    	pointCoordinates.add(new double[] {x, y, z});
     }
 
     @Override
