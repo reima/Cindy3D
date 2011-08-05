46cd86d7f78c15e5a77807e079a52920166356fb
Matthias Reitinger <reitinge@in.tum.de>
2011-08-05 22:01:03 +0200

    Partially commented ModelViewerCamera

--- Cindy3D/src/de/tum/in/cindy3dplugin/jogl/ModelViewerCamera.java
+++ Cindy3D/src/de/tum/in/cindy3dplugin/jogl/ModelViewerCamera.java
@@ -12,21 +12,59 @@ public class ModelViewerCamera {
 	private static final double ROTATE_SENSITIVITY = 0.01;
 	private static final double PAN_SENSITIVITY = 0.05;
 	
+	/**
+	 * Camera orientation
+	 */
 	private Rotation rotation;
+	/**
+	 * Camera position
+	 */
 	private Vector3D position;
+	/**
+	 * Look at vector
+	 */
 	private Vector3D lookAt;
+	/**
+	 * Up vector
+	 */
 	private Vector3D up;
+	/**
+	 * View transformation matrix
+	 */
 	private RealMatrix transform;
 	
-	private double zNear, zFar;
-	private double fieldOfView; // In degrees
+	/**
+	 * Distance of near clipping plane
+	 */
+	private double zNear;
+	/**
+	 * Distance of far clipping plane
+	 */
+	private double zFar;
+	
+	/**
+	 * Field of view in degrees
+	 */
+	private double fieldOfView;
+	/**
+	 * Aspect ratio of the viewport (width / height)
+	 */
 	private double aspectRatio;
+	/**
+	 * Perspective projection matrix
+	 */
 	private RealMatrix perspectiveTransform;
 	
 	private double lodFactor;
 	
+	/**
+	 * View frustum clipping planes
+	 */
 	private Plane[] clippingPlanes = new Plane[6];
 	
+	/**
+	 * Constructs a camera with default parameters.
+	 */
 	public ModelViewerCamera() {
 		rotation = Rotation.IDENTITY;
 		position = new Vector3D(0, 0, 1);
@@ -40,9 +78,12 @@ public class ModelViewerCamera {
 		fieldOfView = 45.0;
 		aspectRatio = 4.0/3.0;
 		perspectiveTransform = MatrixUtils.createRealMatrix(4, 4);
-		updatePerspectiveTransform();		
+		updatePerspectiveTransform();
 	}
 	
+	/**
+	 * Calculates the perspective projection matrix from the camera parameters
+	 */
 	private void updatePerspectiveTransform() {
 		double f = 1.0/Math.tan(Math.toRadians(fieldOfView)*0.5);
 		double nearMinusFar = zNear - zFar;
@@ -54,7 +95,10 @@ public class ModelViewerCamera {
 				/ nearMinusFar);
 		perspectiveTransform.setEntry(3, 2, -1);
 	}
-
+	
+	/**
+	 * Calculates the view transformation matrix from the camera parameters
+	 */
 	protected void updateTransform() {
 		// Lookat
 		Vector3D forward = lookAt.subtract(position);
@@ -75,6 +119,13 @@ public class ModelViewerCamera {
 		transform = lookAtMatrix.multiply(modelRotationMatrix);
 	}
 	
+	/**
+	 * Sets the camera's view parameters.
+	 * 
+	 * @param position Camera's position
+	 * @param lookAt Point the camera is looking at
+	 * @param up Up vector
+	 */
 	public void lookAt(Vector3D position, Vector3D lookAt, Vector3D up) {
 		this.position = position;
 		this.up = up;
@@ -82,11 +133,29 @@ public class ModelViewerCamera {
 		updateTransform();
 	}
 
+	/**
+	 * @return View transform matrix
+	 */
 	public RealMatrix getTransform() {
 		return transform;
 	}
 	
-	public void setPerspective(double fieldOfView, int width, int height, double zNear, double zFar) {
+	/**
+	 * Sets the camera's perspective projection parameters.
+	 * 
+	 * @param fieldOfView
+	 *            Horizontal field of view, in degrees
+	 * @param width
+	 *            Width of viewport
+	 * @param height
+	 *            Height of viewport
+	 * @param zNear
+	 *            Distance of near clipping plane
+	 * @param zFar
+	 *            Distance of far clipping plane
+	 */
+	public void setPerspective(double fieldOfView, int width, int height,
+			double zNear, double zFar) {
 		if (height <= 0)
 			height = 1;
 		this.aspectRatio = (double) width / height;
@@ -94,63 +163,106 @@ public class ModelViewerCamera {
 		this.zNear = zNear;
 		this.zFar = zFar;
 		updatePerspectiveTransform();
-		
-		lodFactor = 2.0 * Math.tan(0.5 * fieldOfView / aspectRatio)
-				/ ((double) height); 
+
+		lodFactor = 2.0 * Math.tan(0.5 * this.fieldOfView / aspectRatio)
+				/ ((double) height);
 	}
 	
+	/**
+	 * @return Perspective projection matrix
+	 */
 	public RealMatrix getPerspectiveTransform() {
 		return perspectiveTransform;
 	}
 	
+	/**
+	 * @return Camera orientation
+	 */
 	public Rotation getRotation() {
 		return rotation;
 	}
 
+	/**
+	 * @param rotation
+	 *            New camera orientation
+	 */
 	public void setRotation(Rotation rotation) {
 		this.rotation = rotation;
 		updateTransform();
 	}
 
+	/**
+	 * @return Camera position
+	 */
 	public Vector3D getPosition() {
 		return position;
 	}
 
+	/**
+	 * @param position
+	 *            New camera position
+	 */
 	public void setPosition(Vector3D position) {
 		this.position = position;
 		updateTransform();
 	}
 
+	/**
+	 * @return Look at vector
+	 */
 	public Vector3D getLookAt() {
 		return lookAt;
 	}
 
+	/**
+	 * @param lookAt
+	 *            New look at vector
+	 */
 	public void setLookAt(Vector3D lookAt) {
 		this.lookAt = lookAt;
 		updateTransform();
 	}
 
+	/**
+	 * @return Up vector
+	 */
 	public Vector3D getUp() {
 		return up;
 	}
 
+	/**
+	 * @param up
+	 *            New up vector
+	 */
 	public void setUp(Vector3D up) {
 		this.up = up;
 		updateTransform();
 	}
 	
+	/**
+	 * @return Field of view in degrees
+	 */
 	public double getFieldOfView() {
 		return fieldOfView;
 	}
 	
+	/**
+	 * @return Aspect ratio of the viewport (width / height)
+	 */
 	public double getAspectRatio() {
 		return aspectRatio;
 	}
 	
+	/**
+	 * @return Distance of near clipping plane
+	 */
 	public double getZNear() {
 		return zNear;
 	}
 	
+	/**
+	 * @return Distance of far clipping plane
+	 */
 	public double getZFar() {
 		return zFar;
 	}
@@ -232,7 +344,7 @@ public class ModelViewerCamera {
 		return clippingPlanes;
 	}
 	
-	public double getWorldSpaceError(float screenSpaceError, float distance) {
+	public double getWorldSpaceError(double screenSpaceError, double distance) {
 		return lodFactor * screenSpaceError * distance;
 	}
 }
