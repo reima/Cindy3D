cc212c5733bc13ad882e529c2328b5bd74e5e147
Jan Sommer <sommerj@in.tum.de>
2011-08-09 17:32:46 +0200

    Implemented segmentation of any kind of lines to improve lod selection

--- Cindy3D/src/de/tum/in/cindy3dplugin/jogl/primitives/renderers/fixedfunc/LineRenderer.java
+++ Cindy3D/src/de/tum/in/cindy3dplugin/jogl/primitives/renderers/fixedfunc/LineRenderer.java
@@ -13,6 +13,7 @@ import de.tum.in.cindy3dplugin.jogl.renderers.JOGLRenderState;
 
 public class LineRenderer extends LineRendererBase {
 	private static final int LOD_COUNT = 8;
+	private static final double LINE_LENGTH = 1;
 	
 	private LODMesh[] meshes = new LODMesh[LOD_COUNT];
 	
@@ -23,21 +24,26 @@ public class LineRenderer extends LineRendererBase {
 		int vertexCount = (loops+1) * slices;
 		int faceCount = 2 * loops * slices;
 		
-		LODMesh mesh = new LODMesh(3, vertexCount, faceCount);
+		LODMesh mesh = new LODMesh(3, vertexCount, faceCount, true);
 		
 		/*
 		 * Generate vertices
 		 */
 		double[] vertex = new double[3];
+		double[] normal;
 		
 		for (int loop = 0; loop < loops+1; ++loop) {
-			double zValue = 2.0 * ((double) loop) / loops;
+			double zValue = LINE_LENGTH * (((double) loop) / loops - 0.5);
 			for (int slice = 0; slice < slices; ++slice) {
 				double angle = ((double) slice) / slices * Math.PI * 2;
 				vertex[0] = Math.cos(angle);
 				vertex[1] = Math.sin(angle);
 				vertex[2] = zValue;
-				mesh.putVertex(vertex);
+				
+				normal = vertex.clone();
+				normal[2] = 0;
+				
+				mesh.putVertex(vertex, normal);
 			}
 		}
 		
@@ -50,19 +56,16 @@ public class LineRenderer extends LineRendererBase {
 		
 		for (int loop = 0;  loop < loops; ++loop) {
 			for (int slice = 0; slice < slices; ++slice) {
-//				mesh.putFace(loopOffset + slice, 
-//							 loopOffset + (slice + 1) % slices,
-//							 nextLoopOffset + slice);
-				mesh.putFace(nextLoopOffset + slice,
-						loopOffset + (slice + 1) % slices,
-						loopOffset + slice);
-				
+				mesh.putFace(loopOffset + slice, 
+							 loopOffset + (slice + 1) % slices,
+							 nextLoopOffset + slice);
 //				mesh.putFace(nextLoopOffset + slice,
-//							 loopOffset + (slice + 1) % slices,
-//							 nextLoopOffset + (slice + 1) % slices);
-				mesh.putFace(nextLoopOffset + (slice + 1) % slices,
-						loopOffset + (slice + 1) % slices,
-						nextLoopOffset + slice);
+//						loopOffset + (slice + 1) % slices,
+//						loopOffset + slice);
+				
+				mesh.putFace(nextLoopOffset + slice,
+							 loopOffset + (slice + 1) % slices,
+							 nextLoopOffset + (slice + 1) % slices);
 			}
 			loopOffset = nextLoopOffset;
 			nextLoopOffset += slices;
@@ -76,8 +79,8 @@ public class LineRenderer extends LineRendererBase {
 	@Override
 	public boolean init(GL gl) {
 		for (int lod = 0; lod < LOD_COUNT; ++lod) {
-			int slices = 2 * lod + 1;
-			int loops = 4 * slices;
+			int slices = 2 * lod + 3;
+			int loops = 1;
 			meshes[lod] = createMesh(gl, loops, slices);
 		}
 
@@ -96,6 +99,7 @@ public class LineRenderer extends LineRendererBase {
 	public void preRender(JOGLRenderState jrs) {
 		GL2 gl2 = jrs.gl.getGL2();
 		gl2.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+		gl2.glEnableClientState(GL2.GL_NORMAL_ARRAY);
 		gl2.glEnable(GL2.GL_NORMALIZE);
 		gl2.glDisable(GL2.GL_CULL_FACE);
 	}
@@ -104,6 +108,7 @@ public class LineRenderer extends LineRendererBase {
 	public void postRender(JOGLRenderState jrs) {
 		GL2 gl2 = jrs.gl.getGL2();
 		gl2.glDisableClientState(GL2.GL_VERTEX_ARRAY);
+		gl2.glDisableClientState(GL2.GL_NORMAL_ARRAY);
 		gl2.glDisable(GL2.GL_NORMALIZE);
 	}
 
@@ -113,6 +118,8 @@ public class LineRenderer extends LineRendererBase {
 		// Get the model view matrix
 		RealMatrix modelView = jrs.camera.getTransform();
 
+		// gl2.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+
 		// All computations are made in camera space, so first transform the two
 		// points of the line into camera space by multiplying with the
 		// modelview matrix
@@ -121,44 +128,58 @@ public class LineRenderer extends LineRendererBase {
 
 		Endpoints endPoints = clipLineAtFrustum(jrs.camera, p1, p2,
 				line.lineType);
-		
-		double distance = Math.min(Util.transformPoint(jrs.camera.getTransform(),
-				endPoints.p1).getNorm()
-				- line.radius,
-				Util.transformPoint(jrs.camera.getTransform(),
-						endPoints.p2).getNorm()
-						- line.radius);
-		
-		double allowedWorldSpaceError = jrs.camera.getWorldSpaceError(
-				jrs.renderHints.getAllowedScreenSpaceError(), distance);
-		
-		double obbLength = Vector3D.distance(endPoints.p1, endPoints.p2) / 2.0;
-		
-		LODMesh mesh = meshes[LOD_COUNT - 1];
-		int lod;
-		for (lod = 0; lod < LOD_COUNT; ++lod) {
-			if (meshes[lod].isSufficient(obbLength,
-					allowedWorldSpaceError)) {
-				mesh = meshes[lod];
-				break;
-			}
-		}
+
+		double totalLength = Vector3D.distance(endPoints.p1, endPoints.p2);
+		int boxes = (int) (Math.ceil(totalLength / LINE_LENGTH));
+		double boxLength = totalLength / boxes;
 
 		// After shifting the end points of the ray/line to the maximal visible
 		// positions, the size and orientation for the OBB is needed
+
+		Vector3D direction = endPoints.p2.subtract(endPoints.p1).normalize()
+				.scalarMultiply(boxLength);
+		endPoints.p2 = endPoints.p1.add(direction);
 		RealMatrix cylinder = buildOBBTransform(endPoints, line.radius);
-		
+
 		gl2.glMatrixMode(GL2.GL_MODELVIEW);
 		gl2.glPushMatrix();
-		gl2.glLoadMatrixf(Util.matrixToFloatArrayTransposed(cylinder), 0);
-		// Rotate cylinder's main axis to x-axis
-		gl2.glRotated(90.0, 0.0, 1.0, 0.0);
-		// Center cylinder on origin
-		gl2.glTranslated(0, 0, -1);
-		
-		mesh.render(gl2);
-		
-		//gl2.glCallList(displayList);
+		gl2.glLoadIdentity();
+
+		for (int box = 0; box < boxes; ++box) {
+
+			double distance = Math
+					.min(Util.transformPoint(jrs.camera.getTransform(),
+							endPoints.p1).getNorm()
+							- line.radius,
+							Util.transformPoint(jrs.camera.getTransform(),
+									endPoints.p2).getNorm()
+									- line.radius);
+
+			double allowedWorldSpaceError = jrs.camera.getWorldSpaceError(
+					jrs.renderHints.getAllowedScreenSpaceError(), distance);
+
+			LODMesh mesh = meshes[LOD_COUNT - 1];
+			int lod;
+			for (lod = 0; lod < LOD_COUNT; ++lod) {
+				if (meshes[lod].isSufficient(line.radius,
+						allowedWorldSpaceError)) {
+					mesh = meshes[lod];
+					break;
+				}
+			}
+
+			gl2.glPushMatrix();
+			gl2.glMultMatrixf(Util.matrixToFloatArrayTransposed(cylinder), 0);
+			// Rotate cylinder's main axis to x-axis
+			gl2.glRotated(90.0, 0.0, 1.0, 0.0);
+
+			mesh.render(gl2);
+			gl2.glPopMatrix();
+			gl2.glTranslated(direction.getX(), direction.getY(),
+					direction.getZ());
+			endPoints.p1 = endPoints.p2;
+			endPoints.p2 = endPoints.p1.add(direction);
+		}
 		gl2.glPopMatrix();
 	}
 }
