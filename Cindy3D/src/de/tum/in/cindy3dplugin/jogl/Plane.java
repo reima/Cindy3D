b800c632a59d94ba476c2260cfaaa98a6ac6101a
Matthias Reitinger <reitinge@in.tum.de>
2011-08-10 13:12:06 +0200

    Adjusted comment style in Plane

--- Cindy3D/src/de/tum/in/cindy3dplugin/jogl/Plane.java
+++ Cindy3D/src/de/tum/in/cindy3dplugin/jogl/Plane.java
@@ -19,9 +19,9 @@ public class Plane {
 	 * Constructs a new plane.
 	 * 
 	 * @param normal
-	 * 			Plane normal
+	 * 			plane normal
 	 * @param position
-	 * 			Coordinates of a point on plane
+	 * 			coordinates of a point on plane
 	 */
 	public Plane(Vector3D normal, Vector3D position) {
 		this(normal, -Vector3D.dotProduct(normal, position));
@@ -31,9 +31,9 @@ public class Plane {
 	 * Constructs a new plane.
 	 * 
 	 * @param normal
-	 * 			Plane normal
+	 * 			plane normal
 	 * @param distance
-	 * 			Distance of plane from origin
+	 * 			distance of plane from origin
 	 */
 	public Plane(Vector3D normal, double distance) {
 		this.normal = normal;
@@ -44,11 +44,11 @@ public class Plane {
 	 * Computes the intersection between a ray and the plane.
 	 * 
 	 * @param rayOrigin
-	 * 			Coordinates of the ray origin
+	 * 			coordinates of the ray origin
 	 * @param rayDirection
-	 * 			Ray direction as a vector
+	 * 			ray direction as a vector
 	 * @return
-	 * 			Distance from origin in given direction to intersection point
+	 * 			distance from origin in given direction to intersection point
 	 */
 	public double intersectRay(Vector3D rayOrigin, Vector3D rayDirection) {
 		double denom = Vector3D.dotProduct(rayDirection, normal);
@@ -65,9 +65,9 @@ public class Plane {
 	 * Computes signed distance between a point and the plane.
 	 * 
 	 * @param point
-	 * 			Point coordinates
+	 * 			point coordinates
 	 * @return
-	 * 			Signed distance from point to plane
+	 * 			signed distance from point to plane
 	 */
 	public double distance(Vector3D point) {
 		return Vector3D.dotProduct(normal, point) + distance;
