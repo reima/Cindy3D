00eb57ba02a7e84a6056369088e9b5c607006f29
Jan Sommer <sommerj@in.tum.de>
2011-08-10 16:42:05 +0200

    Changed JOGLViewer to inherit from MouseAdapter

--- Cindy3D/src/de/tum/in/cindy3dplugin/jogl/JOGLViewer.java
+++ Cindy3D/src/de/tum/in/cindy3dplugin/jogl/JOGLViewer.java
@@ -4,8 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
@@ -40,7 +40,7 @@ import de.tum.in.cindy3dplugin.jogl.renderers.SupersampledFBORenderer;
 /**
  * JOGL implementation of <code>Cindy3DViewer</code>.
  */
-public class JOGLViewer implements Cindy3DViewer, MouseListener,
+public class JOGLViewer extends MouseAdapter implements Cindy3DViewer,
 		MouseMotionListener, MouseWheelListener {
 	private static final double POINT_SCALE = 0.05;
 	
@@ -100,7 +100,6 @@ public class JOGLViewer implements Cindy3DViewer, MouseListener,
 			Util.getLogger().info("Trying to call GLProfile.initSingleton");
 			GLProfile.initSingleton(true);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			Util.getLogger().log(Level.SEVERE, e.toString(), e);
 		}
 		
@@ -161,7 +160,6 @@ public class JOGLViewer implements Cindy3DViewer, MouseListener,
 			
 			this.container.add(canvas, BorderLayout.CENTER);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			Util.getLogger().log(Level.SEVERE, e.toString(), e);
 		}
 	}
@@ -359,34 +357,9 @@ public class JOGLViewer implements Cindy3DViewer, MouseListener,
 		scene.addSphere(new Sphere(cx, cy, cz, radius, appearance
 				.getColor(), appearance.getShininess(), appearance.getAlpha()));
 	}
-	
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseClicked(java.awt.event.MouseEvent)
-	 */
-	@Override
-	public void mouseClicked(MouseEvent e) {
-	}
 
 	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseEntered(java.awt.event.MouseEvent)
-	 */
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseExited(java.awt.event.MouseEvent)
-	 */
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mousePressed(java.awt.event.MouseEvent)
+	 * @see java.awt.event.MouseAdapter#mousePressed(java.awt.event.MouseEvent)
 	 */
 	@Override
 	public void mousePressed(MouseEvent e) {
@@ -395,14 +368,7 @@ public class JOGLViewer implements Cindy3DViewer, MouseListener,
 	}
 
 	/* (non-Javadoc)
-	 * @see java.awt.event.MouseListener#mouseReleased(java.awt.event.MouseEvent)
-	 */
-	@Override
-	public void mouseReleased(MouseEvent e) {
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseMotionListener#mouseDragged(java.awt.event.MouseEvent)
+	 * @see java.awt.event.MouseAdapter#mouseDragged(java.awt.event.MouseEvent)
 	 */
 	@Override
 	public void mouseDragged(MouseEvent e) {
@@ -424,16 +390,7 @@ public class JOGLViewer implements Cindy3DViewer, MouseListener,
 	}
 
 	/* (non-Javadoc)
-	 * @see java.awt.event.MouseMotionListener#mouseMoved(java.awt.event.MouseEvent)
-	 */
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	/* (non-Javadoc)
-	 * @see java.awt.event.MouseWheelListener#mouseWheelMoved(java.awt.event.MouseWheelEvent)
+	 * @see java.awt.event.MouseAdapter#mouseWheelMoved(java.awt.event.MouseWheelEvent)
 	 */
 	@Override
 	public void mouseWheelMoved(MouseWheelEvent e) {
